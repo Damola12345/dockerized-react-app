bb20f73c9eb1ee5ec74cc013dc6e3504de1be5e2
Damola12345 <ajiboladamola789@gmail.com>
2022-03-03 18:41:27 +0100

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { queryByText } = render(<App />);
-  const linkElement = screen.queryByText(/learn react/i);
-  expect(linkElement).not.toBeInTheDocument();
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/blog/i);
+  expect(linkElement).toBeInTheDocument();
 });
